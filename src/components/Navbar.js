e921de014ce98a78a177ad45e627a69e2af7b73c
AyushDevdhe <ayushdevdhe64@gmail.com>
2026-03-27 16:51:14 +0530

    Day2 Tasks Add Delete Functionality Added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Navbar.css';
 
 
-function Navbar(){
-    <div className="navbar">
-        <nav>FocusBoard</nav>
-    </div>
+function Navbar({title}){
+    const [task, setTask] = useState("");
+    const [tasks, setTasks] = useState([]);
+
+    const addTask = () =>{
+        if(task === "") return;
+        setTasks([...tasks, task]);
+        setTask("");
+    };
+
+    const deleteTask = (index) =>{
+        const newTasks = tasks.filter((_,i) => i !== index);
+        setTasks(newTasks);
+    };
+
+    return(
+        <div>
+            <h3>{title}</h3>
+
+            {title = "Tasks" && (
+                
+                <>
+                <input value={task} onChange={(e) => setTask(e.target.value)} placeholder = "Enter Task"></input>
+
+                <button onClick ={addTask}>Add Task</button>
+
+                <ul>
+                    {tasks.map((t,i) => (
+                        <li key={i}>
+                            {t}
+                            <button onClick={() => deleteTask(i)}>X</button>
+                        </li>
+                    ))}
+                </ul>
+
+                </>
+            )}
+        </div>
+    );
 }
 
 
 export default Navbar;
+
+
